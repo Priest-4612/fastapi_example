62e673045e31986bd0fcbaa5c9b2f7a231222787
Priest4612 <priest4612@gmail.com>
2022-05-24 01:06:04 +0300

    Добавил файл настройки окружения

---
+++ etl_pg_to_es/config.py
@@ -0,0 +1,23 @@
+from pydantic import BaseConfig, BaseSettings
+
+
+class Settings(BaseSettings):
+    postgres_db: str
+    postgres_host: str
+    postgres_port: int
+    postgres_user: str
+    postgres_password: str
+    redis_host: str
+    redis_port: int
+    redis_password: str
+    redis_prefix: str
+    elastic_host: str
+    elastic_port: int
+    elastic_scheme: str
+    elastic_user: str
+    elastic_password: str
+    elastic_index: str
+    etl_size_limit: int
+
+    class Config(BaseConfig):
+        env_file = '../infra/env/.env'
